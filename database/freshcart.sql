94a9fee7d3256a810fc71ea3bc444a3c9a40b9d4
Hassan Nadeem <64688959+HassanNadeemWebD@users.noreply.github.com>
2023-05-29 10:24:42 +0500

    commit

--- database/freshcart.sql
+++ database/freshcart.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: May 22, 2023 at 08:06 AM
--- Server version: 10.4.24-MariaDB
--- PHP Version: 8.1.6
+-- Generation Time: May 29, 2023 at 07:24 AM
+-- Server version: 10.4.27-MariaDB
+-- PHP Version: 8.2.0
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -32,14 +32,65 @@ CREATE TABLE `admin` (
   `adminname` varchar(255) NOT NULL,
   `email` varchar(255) NOT NULL,
   `password` varchar(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `admin`
 --
 
 INSERT INTO `admin` (`id`, `adminname`, `email`, `password`) VALUES
-(1, 'test', 'test@gmail.com', 'test123');
+(1, 'test', 'test@gmail.com', 'test123'),
+(3, 'hassan', 'hassan@gmail.com ', '1234');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `category`
+--
+
+CREATE TABLE `category` (
+  `categoryid` int(11) NOT NULL,
+  `icon` varchar(255) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `status` int(11) NOT NULL,
+  `description` varchar(255) NOT NULL,
+  `date` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `category`
+--
+
+INSERT INTO `category` (`categoryid`, `icon`, `name`, `status`, `description`, `date`) VALUES
+(1, 'dairy.svg', 'Bakery', 1, 'asass', '2023-05-24'),
+(3, 'snacks.svg', 'Snacks', 1, 'sanks', '2023-05-24'),
+(4, 'clock.svg', 'clock', 0, 'clock', '2023-05-24'),
+(5, 'gift.svg', 'Gift', 1, 'Gift category', '2023-05-29'),
+(6, 'fish.svg', 'Fish', 1, '', '2023-05-30');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `products`
+--
+
+CREATE TABLE `products` (
+  `productID` int(11) NOT NULL,
+  `catID` int(11) NOT NULL,
+  `productName` varchar(255) NOT NULL,
+  `status` int(11) NOT NULL,
+  `description` text NOT NULL,
+  `price` int(11) NOT NULL,
+  `createdAt` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
+  `img` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `products`
+--
+
+INSERT INTO `products` (`productID`, `catID`, `productName`, `status`, `description`, `price`, `createdAt`, `img`) VALUES
+(1, 3, '5 Star Chocolate', 1, 'Chocolate', 50, '2023-05-29 05:22:37', 'product-img-3.jpg');
 
 --
 -- Indexes for dumped tables
@@ -52,6 +103,19 @@ ALTER TABLE `admin`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Indexes for table `category`
+--
+ALTER TABLE `category`
+  ADD PRIMARY KEY (`categoryid`);
+
+--
+-- Indexes for table `products`
+--
+ALTER TABLE `products`
+  ADD PRIMARY KEY (`productID`),
+  ADD KEY `catID` (`catID`);
+
+--
 -- AUTO_INCREMENT for dumped tables
 --
 
@@ -59,7 +123,29 @@ ALTER TABLE `admin`
 -- AUTO_INCREMENT for table `admin`
 --
 ALTER TABLE `admin`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `category`
+--
+ALTER TABLE `category`
+  MODIFY `categoryid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- AUTO_INCREMENT for table `products`
+--
+ALTER TABLE `products`
+  MODIFY `productID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `products`
+--
+ALTER TABLE `products`
+  ADD CONSTRAINT `products_ibfk_1` FOREIGN KEY (`catID`) REFERENCES `category` (`categoryid`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
